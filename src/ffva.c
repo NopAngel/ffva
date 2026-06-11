e3ffc66169d6481fc38de5d2cb6fd8540e379419
angel¿ <155915830+NopAngel@users.noreply.github.com>
2026-06-11 17:46:13 -0400

    `add _splash.c`

--- src/ffva.c
+++ src/ffva.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../include/tracer.h"
+#include "../include/version.h"
 #include <sys/ptrace.h>
 
-int main(int argc, char **argv) {
+void _splash() {
     printf("\033[1;31mF F\033[1;34m V A \n");
-    printf("\033[1;34mFucking Fast Visual ASM\n");
+    printf("\033[1;34mFucking Fast Visual ASM v%s\n", VERSION_COMPLETE);
 
-    printf("\033[1;90mCreated by: NopAngel         LICENSE GPL-2.0 \033[0m\ \n\n\2");
+    printf("\033[1;90mby: <NopAngel>      LICENSE GPL-2.0 \033[0m\ \n\n\2");
+}
+
+int main(int argc, char **argv) {
+    _splash();
     if(argc < 2) {
         printf("use: %s <prog>\n", argv[0]);
         return 1;
@@ -18,7 +23,7 @@ int main(int argc, char **argv) {
     if(pid == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execvp(argv[1], &argv[1]);
-        perror("\033[1;31mexec fail");
+        perror("\033[1;31mexec fail.");
         exit(1);
     } else {
         trace_all(pid);
